892b112753f7a017c0fbbf87e305beb3583765e5
August7337 <augustin.tarit@gmail.com>
2024-03-14 17:30:48 +0100

    fin de séance

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,56 @@
 
 const int hasherPin = 9;
 
+int pinA = 3; // Connected to CLK on KY-040
+ int pinB = 4; // Connected to DT on KY-040
+ int encoderPosCount = 0;
+ int pinALast;
+ int aVal;
+ boolean bCW;
+
 void setup() {
   Serial.begin(9600);
   pinMode(hasherPin, OUTPUT);
+
+  pinMode (pinA,INPUT);
+ pinMode (pinB,INPUT);
+ /* Read Pin A
+ Whatever state it's in will reflect the last position
+ */
+ pinALast = digitalRead(pinA); 
 }
 
 void loop() {
-  function(hasherPin, 100);
+  function(hasherPin, encoderPosCount);
+
+  aVal = digitalRead(pinA);
+ if (aVal != pinALast){ // Means the knob is rotating
+ // if the knob is rotating, we need to determine direction
+ // We do that by reading pin B.
+ if (digitalRead(pinB) != aVal) { // Means pin A Changed first - We're Rotating Clockwise
+ if (encoderPosCount < 100)
+ {
+  encoderPosCount ++;
+ }
+ 
+ bCW = true;
+ } else {// Otherwise B changed first and we're moving CCW
+ bCW = false;
+ if (encoderPosCount > 0)
+ {
+   encoderPosCount--;
+ }
+
+ }
+ Serial.print ("Rotated: ");
+ if (bCW){
+ Serial.println ("clockwise");
+ }else{
+ Serial.println("counterclockwise");
+ }
+ Serial.print("Encoder Position: ");
+ Serial.println(encoderPosCount);
+
+ }
+ pinALast = aVal;
 }
