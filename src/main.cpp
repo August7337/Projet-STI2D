14eeb87056c9ad7ebe0a55d6931b910b29210a61
August7337 <augustin.tarit@gmail.com>
2024-04-04 21:56:58 +0200

    potentiometre

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
 #include <Arduino.h>
 #include "hasher.h"
-#include "potentiometer.h"
+#include "RotaryEncoder.h"
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-const int hasherPin = 9;
-
-int pinA = 3; // Connected to CLK on KY-040
-int pinB = 4; // Connected to DT on KY-040
-int pinC = 5;
-int pinALast;
+const int hasherPin = 10;
 
 #define LCD_ADDR 0x27 
 
@@ -21,14 +16,30 @@ int pinALast;
 // Création d'une instance de l'écran LCD
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);  
 
+
+int Counter = 0, LastCount = 0; //uneeded just for test
+void RotaryChanged(); //we need to declare the func above so Rotary goes to the one below
+
+RotaryEncoder Rotary(&RotaryChanged, 2, 3, 4); // Pins 2 (DT), 3 (CLK), 4 (SW)
+
+
+void RotaryChanged()
+{
+  const unsigned int state = Rotary.GetState();
+  
+  if (state & DIR_CW) 
+    Counter++;
+    
+  if (state & DIR_CCW)  
+    Counter--;    
+}
+
+
+
 void setup() {
+    Rotary.setup();  
     Serial.begin(9600);
     pinMode(hasherPin, OUTPUT);
-
-    pinMode (pinA,INPUT);
-    pinMode (pinB,INPUT);
-    pinMode (pinC,INPUT);
-    pinALast = digitalRead(pinA); 
 
     lcd.init();   
     lcd.backlight();
@@ -37,42 +48,41 @@ void setup() {
 
 int lastPot=0;
 int pot;
+int potMenu;
 
 int menuPos = 1;
 
 void fanMenu();
 void fanMenu(){
-    pot = Potentiometer(pinA, pinB, pinALast, 0, 100);
-    Hasher(hasherPin, pot);
+    Hasher(hasherPin, Counter);
 
-    if (lastPot == pot)
+    if (LastCount != Counter)  
     {
+        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Ventilateur 1");
         lcd.setCursor(0,1);
-        lcd.print(String(pot));
-        lcd.print(" %");
-        lcd.clear();
+        lcd.print(String(Counter));
+        lcd.print(" %");  
     }
-    lastPot = pot;
 }
 
 void loop() {
-
     if (menuPos == 1)
     {
         lcd.setCursor(0,0);
         lcd.print("Menu principal");
-
-        lcd.clear();
-        pot = Potentiometer(pinA, pinB, pinALast, 0, 100);
-        Serial.println(digitalRead(pinC));
-        //Serial.println(pot);
-        if (digitalRead(pinC) == 0)
+        
+        if (Rotary.GetButtonDown())
         {
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Ventilateur 1");
+            lcd.setCursor(0,1);
+            lcd.print(String(Counter));
+            lcd.print(" %");  
             menuPos = 11;
         }
-        Hasher(hasherPin, pot);
     }
     else if (menuPos == 11){
         fanMenu();
