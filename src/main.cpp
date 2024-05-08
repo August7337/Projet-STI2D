7436c00b980cb8b64c213c56e01bd777c7f5be5f
August7337 <augustin.tarit@gmail.com>
2024-05-08 18:22:17 +0200

    08/05/2024

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// Importation des fichiers
 #include <Arduino.h>
 #include "hasher.h"
 #include "RotaryEncoder.h"
@@ -5,6 +6,7 @@
 #include <LiquidCrystal_I2C.h>
 
 
+// Declaration des variables
 const int fan1HasherPin = 9;
 const int tempHasherPin = 10;
 const int motorHasherPin = 11;
@@ -14,25 +16,23 @@ const int motorHasherPin = 11;
 // Nombre de colonnes et de lignes de l'écran LCD
 #define LCD_COLS 16
 #define LCD_ROWS 4
-
 // Création d'une instance de l'écran LCD
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);  
-
+// Variables du potentiometre
 int lastPot=0;
 int pot;
 int potMenu;
 int menuPos = 1;
-
+// Variables pour les menus
 bool needRefresh = false;
-
 int settingsCounter = 0, lastSettingsCount = 0;
-int fan1Count = 0, lastFan1Count = 0;
-int lastTempCount = 0, tempCount = 0;
-int lastMotorCount = 0, motorCount = 0;
+int fan1Count = 0, lastFan1Count = 0, fan1Target = 0;
+int lastTempCount = 0, tempCount = 0, tempTarget = 0;
+int lastMotorCount = 0, motorCount = 0, motorTarget = 0;
 
+// Fonction de changement des variables, lors de la rotation du potentiometre
 void RotaryChanged();
 RotaryEncoder Rotary(&RotaryChanged, 2, 3, 4); // Pins 2 (DT), 3 (CLK), 4 (SW)
-
 void RotaryChanged()
 {
   const unsigned int state = Rotary.GetState();
@@ -77,6 +77,7 @@ void RotaryChanged()
   }
 }
 
+// Interface et logique du menu principal
 void mainMenu();
 void mainMenu(){
     lcd.clear();
@@ -90,25 +91,7 @@ void mainMenu(){
     }
 }
 
-void setup() {
-    Rotary.setup();  
-    Serial.begin(9600);
-    pinMode(fan1HasherPin, OUTPUT);
-    digitalWrite(fan1HasherPin, LOW);
-
-    pinMode(tempHasherPin, OUTPUT);
-    digitalWrite(tempHasherPin, LOW);
-
-    pinMode(motorHasherPin, OUTPUT);
-    digitalWrite(motorHasherPin, LOW);
-    
-    lcd.init();   
-    lcd.backlight();
-    lcd.setCursor(0,0);
-
-    mainMenu();
-}
-
+// Interface et logique des parametres
 void settingsMenu();
 void settingsMenu(){
 
@@ -173,10 +156,10 @@ void settingsMenu(){
     }
 }
 
-
+// Interface et logique du ventilateur 1
 void fanMenu();
 void fanMenu(){
-
+    Hasher(fan1HasherPin, fan1Count);
     if (lastFan1Count != fan1Count || needRefresh)  
     {
         needRefresh = false;
@@ -195,13 +178,14 @@ void fanMenu(){
     {
         needRefresh = true;
         menuPos = 11;
-        
+        fan1Target = fan1Count;
     }
 }
 
+// Interface et logique de la temperature
 void tempMenu();
 void tempMenu(){
-
+    Hasher(tempHasherPin, tempCount);
     if (lastTempCount != tempCount || needRefresh)  
     {
         needRefresh = false;
@@ -220,13 +204,14 @@ void tempMenu(){
     {
         needRefresh = true;
         menuPos = 11;
-        
+        tempTarget = tempCount;
     }
 }
 
+// Interface et logique du moteur
 void motorMenu();
 void motorMenu(){
-
+    Hasher(motorHasherPin, motorCount);
     if (lastMotorCount != motorCount || needRefresh)  
     {
         needRefresh = false;
@@ -245,11 +230,11 @@ void motorMenu(){
     {
         needRefresh = true;
         menuPos = 11;
-        
+        motorTarget = motorCount;
     }
 }
 
-
+// Fonction exécuté en boucle
 void loop() {
     if (menuPos == 1)
     {
@@ -268,9 +253,29 @@ void loop() {
         fanMenu();
     }
 
-    // Hasher section
-    Hasher(fan1HasherPin, fan1Count);
-    Hasher(tempHasherPin, tempCount);
-    Hasher(motorHasherPin, motorCount);
-    Serial.println(motorCount);
+    // Section du controle des éléments 12v
+    //Hasher(fan1HasherPin, fan1Target);
+    //Hasher(tempHasherPin, tempTarget);
+    //Hasher(motorHasherPin, motorTarget);
+    //Hasher(fan1HasherPin, fan1Count);
+}
+
+// Fonction exécuté
+void setup() {
+    Rotary.setup();  
+    Serial.begin(9600);
+    pinMode(fan1HasherPin, OUTPUT);
+    digitalWrite(fan1HasherPin, LOW);
+
+    pinMode(tempHasherPin, OUTPUT);
+    digitalWrite(tempHasherPin, LOW);
+
+    pinMode(motorHasherPin, OUTPUT);
+    digitalWrite(motorHasherPin, LOW);
+    
+    lcd.init();   
+    lcd.backlight();
+    lcd.setCursor(0,0);
+
+    mainMenu();
 }
